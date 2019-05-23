e9a8f79b24d6495b2586044d8bd8f22c31763e01
enduroRider <luki.bigos@gmail.com>
2019-05-23 15:08:09 +0200

    jome component part 2

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
 
-import homeImg from '../../assets/img/movie.png'
+import homeImg from "../../assets/img/movie.png";
+import "./Home.css";
 
 export default function Home() {
   return (
-    <div>
-      <img src={homeImg} alt=""/>
+    <div className="main-content">
+      <h2>Oscars Winners</h2>
+      <img src={homeImg} alt="" />
+      <section className="thumbnails">
+        <div className="thumbnail-1" />
+        <div className="thumbnail-2" />
+        <div className="thumbnail-3" />
+      </section>
     </div>
-  )
+  );
 }
