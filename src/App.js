8896c950f25a29a7a72248e285248c935ffc0069
enduroRider <luki.bigos@gmail.com>
2019-05-23 09:25:31 +0200

    progress on home component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from './Components/Navbar/Navbar';
+import Home from './Components/Home/Home';
 
 import './App.css';
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Home/>
     </div>
   );
 }
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import homeImg from '../../assets/img/movie.png'
+
+export default function Home() {
+  return (
+    <div>
+      <img src={homeImg} alt=""/>
+    </div>
+  )
+}
